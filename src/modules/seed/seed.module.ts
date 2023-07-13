0af248d0c91b7d38cb5f5b098ccf836d589cf9d3
Matheus Nachtigall <matheus.nachtigall@dentsu.com>
2023-07-13 16:36:22 -0300

    first prisma service module on seed

--- src/modules/seed/seed.module.ts
+++ src/modules/seed/seed.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PrismaService } from '../../database/PrismaService';
 
 @Module({
   controllers: [SeedController],
-  providers: [SeedService]
+  providers: [SeedService, PrismaService],
 })
 export class SeedModule {}
